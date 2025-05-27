789800dadac5b41e17937927af74ba535519db83
OfirIsHere <Ofirdoron19@gmail.com>
2025-05-27 15:42:43 +0300

    fix weird bug with the packages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,16 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+export const getAllVisits = onRequest((request, response) => {
+    logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});
