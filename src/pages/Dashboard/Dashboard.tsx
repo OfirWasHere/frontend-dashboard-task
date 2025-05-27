d0e0842fefe3450ab0e1c9974c6568ea38b39bcd
OfirIsHere <Ofirdoron19@gmail.com>
2025-05-27 15:27:51 +0300

    remove eslint breaking with typescript

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@mui/material";
 import React from "react";
 import useAuth from "../../hooks/useAuth";
+import { GetAllDataFromFireStore } from "../../services/dashboardService";
 
 function Dashboard() {
   const { firebaseLogout } = useAuth();
+
   return (
     <div>
       <Button variant="contained" onClick={() => firebaseLogout()}>
         log out
       </Button>
+      <Button variant="contained" onClick={GetAllDataFromFireStore}></Button>
     </div>
   );
 }
---
+++ src/services/dashboardService.ts
@@ -0,0 +1,19 @@
+
+export const GetAllDataFromFireStore = async (data: any) => {
+    try {
+        const result = await fetch(' http://127.0.0.1:5001/fullstack-home-task/us-central1/getAllVisits', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await result.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error calling Firebase function:', error);
+    }
+}
+export const addDataToFireStore = async (data: any) => { }
+export const removeDataFromFireStore = async (data: any) => { }
+export const editDataFromFireStore = async (data: any) => { }
+export const DeleteDataFromFireStore = async (data: any) => { }
